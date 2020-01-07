370b59b885a80b223350ee3f73fb69a521cb0c48
BitBitcode <1378028451@qq.com>
2020-01-07 18:00:59 +0800

    新增：6

---
+++ 6、计算学生平均身高/6、计算学生平均身高.c
@@ -0,0 +1,26 @@
+﻿//《C语言从入门到精通》P28
+// 第二章 C语言基础
+// 2.1 数据类型
+// 6、计算学生平均身高.c
+
+#include <stdio.h>
+
+void main()
+{
+	float height_Kiana;
+	float height_Mei;
+	float height_Bronya;
+	float average;
+
+	printf("请输入琪亚娜的身高：");
+	scanf_s("%f", &height_Kiana);
+	
+	printf("请输入芽衣的身高：");
+	scanf_s("%f", &height_Mei);
+
+	printf("请输入布洛妮娅的身高：");
+	scanf_s("%f", &height_Bronya);
+
+	average = (height_Kiana + height_Mei + height_Bronya) / 3;
+	printf("御三家的平均身高是(→_→)：%f", average);
+}
