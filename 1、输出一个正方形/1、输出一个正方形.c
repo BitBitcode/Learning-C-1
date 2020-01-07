48888ac1b2caaa5d363354ccdca13c8c505c688d
BitBitcode <1378028451@qq.com>
2020-01-07 17:31:11 +0800

    添加项目文件。

---
+++ 1、输出一个正方形/1、输出一个正方形.c
@@ -0,0 +1,14 @@
+﻿//《C语言从入门到精通》P27
+// 第一章 初识C语言
+// 1.2 认识C程序
+// 1、输出一个正方形
+
+#include<stdio.h>
+main()
+{
+	printf("* * * * *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("* * * * *\n");
+}
