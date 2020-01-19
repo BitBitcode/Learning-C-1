6408eb7f26d86650b1f478039eecca46ecfdd881
BitBitcode <1378028451@qq.com>
2020-01-19 21:35:46 +0800

    2

---
+++ 22、用 if 嵌套语句选择日程安排/22、用 if 嵌套语句选择日程安排.c
@@ -0,0 +1,46 @@
+﻿//《C语言从入门到精通》P85
+// 第三章 顺序与选择结构程序设计
+// 3.4 if 语句
+// 22、用 if 嵌套语句选择日程安排
+
+#include<stdio.h>
+
+void main()
+{
+	int day = 0;
+
+	int Monday = 1;
+	int Tuesday = 2;
+	int Wednesday = 3;
+	int Thursday = 4;
+	int Friday = 5;
+	int Saturday = 6;
+	int Sunday = 7;
+
+	printf("请输入星期：（数字1~7）\n");
+	scanf_s("%d", &day);
+
+	if(day>Friday)
+	{
+		printf("嗯，今天是周末\n");
+		if (day == Saturday)
+		{
+			printf("和朋友去逛街！\n");
+		}
+		else
+		{
+			printf("去陪爸爸妈妈！\n");
+		}
+	}
+	else
+	{
+		if (day == Monday)
+		{
+			printf("新的一周开始啦！\n");
+		}
+		else
+		{
+			printf("周内呢，今天也要元气满满哦\n");
+		}
+	}
+}
---
+++ 23、判断偶数/23、判断奇数还是偶数.c
@@ -0,0 +1,22 @@
+﻿//《C语言从入门到精通》P85
+// 第三章 顺序与选择结构程序设计
+// 3.4 if 语句
+// 23、判断偶数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+
+#include<stdio.h>
+
+void main()
+{
+	int num;
+
+	printf("请输入一个整数：\n");
+	scanf_s("%d", &num);
+	if (num % 2 == 0)
+	{
+		printf("你输入的是偶数！\n");
+	}
+	else
+	{
+		printf("你输入的是奇数！\n");
+	}
+}
